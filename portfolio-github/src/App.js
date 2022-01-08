e2bdf6a364572999a2471cd28dd8fb5204ee3bfe
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-01-08 14:58:42 -0300

    Resolução exercício 3 semana 9

--- portfolio-github/src/App.js
+++ portfolio-github/src/App.js
@@ -1,21 +1,23 @@
 import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 
 const lista_repositorios = [];
-  lista_repositorios[1] = {id: 1, titulo: "Título do primeiro item", descricao: "Descrição do primeiro item"};
-  lista_repositorios[2] = {id: 2, titulo: "Título do segundo item", descricao: "Descrição do segundo item"};
-  lista_repositorios[3] = {id: 3, titulo: "Título do terceiro item", descricao: "Descrição do terceiro item"};
-  lista_repositorios[4] = {id: 4, titulo: "Título do quarto item", descricao: "Descrição do quarto item"};
-  lista_repositorios[5] = {id: 5, titulo: "Título do quinto item", descricao: "Descrição do quinto item"};
+lista_repositorios[1] = {id: 1, titulo: "Título do primeiro item", descricao: "Descrição do primeiro item"};
+lista_repositorios[2] = {id: 2, titulo: "Título do segundo item", descricao: "Descrição do segundo item"};
+lista_repositorios[3] = {id: 3, titulo: "Título do terceiro item", descricao: "Descrição do terceiro item"};
+lista_repositorios[4] = {id: 4, titulo: "Título do quarto item", descricao: "Descrição do quarto item"};
+lista_repositorios[5] = {id: 5, titulo: "Título do quinto item", descricao: "Descrição do quinto item"};
 
 export function App() {
+  const [repositorios, setRepositorio] = useState(lista_repositorios);
   return (
     <section>
       <h1>Meu portfólio Github!</h1>
       <Resumo name='Laci Jr' imagem='https://github.com/LaciJr.png'/>
       {
-      lista_repositorios.length === 0 ? <p>Nenhum repositório disponível.</p> : (
-        lista_repositorios.map((valor) => (
+      repositorios.length === 0 ? <p>Nenhum repositório disponível.</p> : (
+        repositorios.map((valor) => (
           <Repositorio destacar={valor.id === 1 ? true : false} titulo={`${valor.titulo}`} descricao={`${valor.descricao}`}/>
         ))
       )
@@ -23,7 +25,6 @@ export function App() {
     </section>
   );
 }
-
 function Resumo(props) {
   return (
     <div className={styles.card}>
