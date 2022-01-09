48486f43579091da2aef0bfaac77ec3e0da096c9
Laci Leal Junior <laci_leal@estudante.sc.senai.br>
2022-01-09 20:47:10 -0300

    Resolução exercício 7 semana 9

--- portfolio-github/src/App.js
+++ portfolio-github/src/App.js
@@ -4,15 +4,16 @@ import styles from './App.module.css';
 import { mapToRepoObject } from './data/data-utils';
 
 const lista_repositorios = [];
-lista_repositorios[0] = {id: 0, titulo: "Título do primeiro item", descricao: "Descrição do primeiro item"};
-lista_repositorios[1] = {id: 1, titulo: "Título do segundo item", descricao: "Descrição do segundo item"};
-lista_repositorios[2] = {id: 2, titulo: "Título do terceiro item", descricao: "Descrição do terceiro item"};
-lista_repositorios[3] = {id: 3, titulo: "Título do quarto item", descricao: "Descrição do quarto item"};
-lista_repositorios[4] = {id: 4, titulo: "Título do quinto item", descricao: "Descrição do quinto item"};
+lista_repositorios[0] = {id: 1, titulo: "Título do primeiro item", descricao: "Descrição do primeiro item"};
+lista_repositorios[1] = {id: 2, titulo: "Título do segundo item", descricao: "Descrição do segundo item"};
+lista_repositorios[2] = {id: 3, titulo: "Título do terceiro item", descricao: "Descrição do terceiro item"};
+lista_repositorios[3] = {id: 4, titulo: "Título do quarto item", descricao: "Descrição do quarto item"};
+lista_repositorios[4] = {id: 5, titulo: "Título do quinto item", descricao: "Descrição do quinto item"};
 
 export function App() {
   const [repositorios, setRepositorio] = useState(lista_repositorios);
-  const [idSelecionado, setId] = useState(2)
+  const [idSelecionado, setId] = useState(2);
+  const [nomeUsuario, setNomeUsuario] = useState('');
 
   useEffect(() => {
     fetch(`https://api.github.com/users/LaciJr/repos`)
@@ -23,11 +24,17 @@ export function App() {
       setId(repoResultado[0].id);
     })
   },[])
+
+  function handleNomeUsuario(valor) {
+    setNomeUsuario(valor);
+  }
   
   return (
     <section>
       <h1>Meu portfólio Github!</h1>
       <Resumo name='Laci Jr' imagem='https://github.com/LaciJr.png'/>
+      <input type="text" onChange={event => handleNomeUsuario(event.target.value)} value={nomeUsuario}/>
+      <button>Buscar</button>
       {
       repositorios.length === 0 ? <p>Nenhum repositório disponível.</p> : (
         repositorios.map((valor) => (
